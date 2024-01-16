23d8998f9da84013927e1697d4ebb2e106563e26
MuhammadHaroonGill <156077574+MuhammadHaroonGill@users.noreply.github.com>
2024-01-16 11:30:17 +0500

    Add files via upload

---
+++ Haroon/src/haroon/Task.java
@@ -0,0 +1,10 @@
+package haroon;
+
+public class Task {
+	public static void main(String[] args) {
+	        // Print normal text
+	       System.out.println("Hello, this is Venmer");        
+	 }
+}
+
+
